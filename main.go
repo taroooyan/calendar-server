1d9fb4d385b543f2d23a3bdbf8295a9fec5601a2
taroooyan <taroooyan@gmail.com>
2017-03-14 07:56:32 +0900

    run go fmt

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
-import (
-  "fmt"
-  "net/http"
-  "os"
-)
-func ShowICS(w http.ResponseWriter, r *http.Request) {
-  file, _ := os.Open("basic.ics")
-  defer file.Close()
 
-  buf := make([]byte, 1024)
-  for {
-    n, _ := file.Read(buf)
-    if n == 0 {
-      break
-    }
-    fmt.Fprintf(w, string(buf[:n]))
-  }
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func ShowICS(w http.ResponseWriter, r *http.Request) {
+	file, _ := os.Open("basic.ics")
+	defer file.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, _ := file.Read(buf)
+		if n == 0 {
+			break
+		}
+		fmt.Fprintf(w, string(buf[:n]))
+	}
 }
 func main() {
-  http.HandleFunc("/calendar.ics", ShowICS)
-  http.ListenAndServe(":80", nil)
+	http.HandleFunc("/calendar.ics", ShowICS)
+	http.ListenAndServe(":80", nil)
 }
