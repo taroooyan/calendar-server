7201b583a8897c3065b47cee7d29a9beded6c0f0
taroooyan <taroooyan@gmail.com>
2017-03-20 23:53:56 +0900

    delete unuse func

--- ics/ics.go
+++ ics/ics.go
@@ -102,3 +102,25 @@ func PrintICS(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+/*
+func readICS(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("basic.ics")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, string(buf))
+	}
+}
+*/
--- main.go
+++ main.go
@@ -7,30 +7,7 @@ import (
 	"os"
 )
 
-func readICS(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open("basic.ics")
-	if err != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-
-	buf := make([]byte, 1024)
-	for {
-		n, err := file.Read(buf)
-		if n == 0 {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
-		fmt.Fprintf(w, string(buf))
-	}
-}
-
 func main() {
-	// takeArticle()
-
 	http.HandleFunc("/calendar.ics", ics.PrintICS)
 	http.ListenAndServe(":8080", nil)
 }
